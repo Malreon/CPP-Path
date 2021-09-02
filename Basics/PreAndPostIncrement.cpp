9c27fd4622fdc93ac923a478452674c493831669
Estra <23023492+Malreon@users.noreply.github.com>
2021-09-02 15:02:33 -0500

    Add files via upload

---
+++ Basics/PreAndPostIncrement.cpp
@@ -0,0 +1,12 @@
+// Demonstration of pre-increment and post-increment operators
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ int A = 1,
+     B = 1;
+     cout << "this is a pre-increment ++A: " << ++A << endl;
+     cout << "this is a post-increment B++: " << B++ << endl;
+	return 0;		
+}
