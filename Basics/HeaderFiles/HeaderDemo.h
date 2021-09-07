91a92015de3b11b2c78146f0a7065d02e6685c77
Estra <23023492+Malreon@users.noreply.github.com>
2021-09-06 23:21:04 -0500

    Add files via upload

---
+++ Basics/HeaderFiles/HeaderDemo.h
@@ -0,0 +1,7 @@
+//header file demonstration, used for the declaration of functions, prototypes, constructors, destructors, classes, and global variables
+#include <iostream>
+
+
+int Area(int x, int y){
+  return x*y;
+}
