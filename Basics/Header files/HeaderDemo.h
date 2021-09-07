d1132e0b944071f8829d0bca5d3b4a2ec02511cd
Estra <23023492+Malreon@users.noreply.github.com>
2021-09-06 23:20:32 -0500

    Delete HeaderDemo.h

--- Basics/Header files/HeaderDemo.h
+++
@@ -1,7 +0,0 @@
-//header file demonstration, used for the declaration of functions, prototypes, constructors, destructors, classes, and global variables
-#include <iostream>
-
-
-int Area(int x, int y){
-  return x*y;
-}
