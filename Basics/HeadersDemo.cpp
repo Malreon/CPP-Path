c468780d4ec5991768fa919adf790764528893c7
Estra <23023492+Malreon@users.noreply.github.com>
2021-09-06 23:22:20 -0500

    Add files via upload

---
+++ Basics/HeadersDemo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include"headerFiles/HeaderDemo.h" // use quotations for user made header files
+
+using namespace std;
+//main  functions call the area function from the user defined header file
+int main(){
+  cout << area( 12, 5);
+  return 0;
+}
